a438f250f6bbf51b5194c95af8e02690a100747e
Aiden Sanders <aidensandersbiz@gmail.com>
2025-02-20 11:07:08 -0700

    Sign-up validation (unfinished) Dummy DB creation

--- CBL_CasinoSuite/Pages/SignUp.cshtml.cs
+++ CBL_CasinoSuite/Pages/SignUp.cshtml.cs
@@ -1,9 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using CBL_CasinoSuite.Data.Interfaces;
+using CBL_CasinoSuite.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace CBL_CasinoSuite.Pages;
 
-public class SignUp : PageModel {
-    public void OnGet() {
-        
+[BindProperties]
+public class SignUp : PageModel
+{
+    [Required, MinLength(5, ErrorMessage = "Your username must be at least 5 characters"), MaxLength(20, ErrorMessage = "Your username cannot exceed 20 characters")]
+    public string NewUsername { get; set; }
+    [Required, MinLength(6, ErrorMessage = "Your password must be at least 6 characters"), MaxLength(20, ErrorMessage = "Your password cannot exceed 20 characters")]
+    public string NewPassword { get; set; }
+    [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+    public string ConfirmPassword { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public string UsernameWarning { get; set; } = "";
+
+    public SignUp(IDal dal, IGameList gameList, IUser userSingleton)
+    {
+        _dal = dal;
+        _gameList = gameList;
+        _userSingleton = userSingleton;
     }
+
+    public void OnGet()
+    {
+
+    }
+
+    public IActionResult OnPostSignUp()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        if (string.IsNullOrEmpty(_dal.GetUser(NewUsername).Username))
+        {
+            User newUser = new Data.Models.User(NewUsername, NewPassword, _gameList);
+            _dal.AddUser(newUser);
+            _userSingleton.SetUser(newUser);
+
+            return RedirectToPage("/Index");
+        }
+
+        return RedirectToAction("Get", new { UsernameWarning = "That username is already taken" });
+    }
+
+    private IDal _dal;
+    private IGameList _gameList;
+    private IUser _userSingleton;
 }
