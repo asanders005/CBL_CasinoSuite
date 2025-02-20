ae20a42c09becec00f97e6f6ac8ffc05e32c69f4
Aiden Sanders <aidensandersbiz@gmail.com>
2025-02-20 10:24:16 -0700

    Adding User Password Update to IDal

--- CBL_CasinoSuite/Data/Models/User.cs
+++ CBL_CasinoSuite/Data/Models/User.cs
@@ -4,6 +4,7 @@ namespace CBL_CasinoSuite.Data.Models
 {
     public class User
     {
+        []
         public readonly string Username;
         public float CurrentBalance { get; private set; }
         public List<GameStats> GameStatistics { get; private set; } = new List<GameStats>();
