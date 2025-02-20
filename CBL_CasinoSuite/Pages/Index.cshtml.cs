0065713c1fc08b6c291ec26fa5df4b01661ab6ad
verycoolgithubaccount <tposselt@student.neumont.edu>
2025-02-20 11:41:28 -0700

    pull from master

--- CBL_CasinoSuite/Pages/Index.cshtml.cs
+++ CBL_CasinoSuite/Pages/Index.cshtml.cs
@@ -16,8 +16,8 @@ namespace CBL_CasinoSuite.Pages
 
         public void OnGet()
         {
-            User newUser = new User("Newothan McUser", "NOTpassword1234", new GameList());
-            dbDal.AddUser(newUser);
+            //User newUser = new User("Newothan McUser", "NOTpassword1234", new GameList());
+            //dbDal.AddUser(newUser);
         }
     }
 }
