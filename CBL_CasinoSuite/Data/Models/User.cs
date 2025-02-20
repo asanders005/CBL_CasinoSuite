fe65fbb411e5af2c780ef898ee10ea53e37beb8e
Aiden Sanders <152341389+asanders005@users.noreply.github.com>
2025-02-20 10:25:34 -0700

    Update User.cs

--- CBL_CasinoSuite/Data/Models/User.cs
+++ CBL_CasinoSuite/Data/Models/User.cs
@@ -4,7 +4,6 @@ namespace CBL_CasinoSuite.Data.Models
 {
     public class User
     {
-        []
         public readonly string Username;
         public float CurrentBalance { get; private set; }
         public List<GameStats> GameStatistics { get; private set; } = new List<GameStats>();
