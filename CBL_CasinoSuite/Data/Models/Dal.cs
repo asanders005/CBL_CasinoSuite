515fd2d4b3499a3f21ec602a4402ae741b62817d
verycoolgithubaccount <tposselt@student.neumont.edu>
2025-02-25 10:43:55 -0700

    Update user singleton when updating the database

--- CBL_CasinoSuite/Data/Models/Dal.cs
+++ CBL_CasinoSuite/Data/Models/Dal.cs
@@ -7,6 +7,14 @@ namespace CBL_CasinoSuite.Data.Models
 {
     public class Dal : IDal
     {
+        private IUser userSingleton;
+        public Dal(IUser user)
+        {
+            userSingleton = user;
+        }
+
+
+
         private static MongoClient dbClient = new MongoClient("mongodb+srv://verycoolmongoaccount:4!5$pVHyYSEb!L@users.sb3bg.mongodb.net/?retryWrites=true&w=majority&appName=Users");
 
         private static IMongoDatabase database = dbClient.GetDatabase("CasinoSuite");
@@ -36,35 +44,98 @@ namespace CBL_CasinoSuite.Data.Models
 
         public void UpdateUser(string username, User user)
         {
+            // Update the database
+
             var update = Builders<User>.Update
                 .Set(u => u.CurrentBalance, user.CurrentBalance)
                 .Set(u => u.GameStatistics, user.GameStatistics);
 
             collection.UpdateOne(u => u.Username == username, update);
+
+            // Update the user singleton
+
+            User userSingletonUpdate = userSingleton.GetUser();
+
+            userSingletonUpdate.CurrentBalance = user.CurrentBalance;
+            userSingletonUpdate.GameStatistics = new List<GameStats>(user.GameStatistics);
+
+            userSingleton.SetUser(userSingletonUpdate);
         }
 
         public void UpdateUserBalance(string username, float balance)
         {
+            // Update the database
+
             var update = Builders<User>.Update.Set(u => u.CurrentBalance, balance);
             collection.UpdateOne(u => u.Username == username, update);
+
+            // Update the user singleton
+
+            User userSingletonUpdate = userSingleton.GetUser();
+            userSingletonUpdate.CurrentBalance = balance;
+            userSingleton.SetUser(userSingletonUpdate);
         }
 
         public void UpdateUserPassword(string username, string password)
         {
+            // Update the database
+
             var update = Builders<User>.Update.Set(u => u.Password, password);
             collection.UpdateOne(u => u.Username == username, update);
+
+            // Update the user singleton
+
+            User userSingletonUpdate = userSingleton.GetUser();
+            userSingletonUpdate.Password = password;
+            userSingleton.SetUser(userSingletonUpdate);
         }
 
         public void UpdateUserStatistics(string username, GameStats gameStatistics)
         {
-            var filter = Builders<User>.Filter.And(
-                Builders<User>.Filter.Eq(u => u.Username, username),
-                Builders<User>.Filter.Eq("GameStatistics._GameName", gameStatistics._GameName) // Find the correct game
-            );
+            // Update the database
 
-            var update = Builders<User>.Update.Set("GameStatistics.$", gameStatistics);
+            User dbUser = GetUser(username);
 
-            collection.UpdateOne(filter, update);
+            if (dbUser.GameStatistics.Where(g => g._GameName == gameStatistics._GameName).Count() > 0)
+            {
+                var filter = Builders<User>.Filter.And(
+                    Builders<User>.Filter.Eq(u => u.Username, username),
+                    Builders<User>.Filter.Eq("GameStatistics._GameName", gameStatistics._GameName) // Find the correct game
+                );
+
+                var update = Builders<User>.Update.Set("GameStatistics.$", gameStatistics);
+
+                collection.UpdateOne(filter, update);
+            }
+            else
+            {
+                dbUser.GameStatistics.Add(gameStatistics);
+                UpdateUser(username, dbUser);
+            }
+
+            
+
+            // Update the user singleton
+
+            User userSingletonUpdate = userSingleton.GetUser();
+
+            bool foundUserStatsGame = false;
+            for (int i = 0; i < userSingletonUpdate.GameStatistics.Count; i++)
+            {
+                if (userSingletonUpdate.GameStatistics[i]._GameName == gameStatistics._GameName)
+                {
+                    userSingletonUpdate.GameStatistics[i] = gameStatistics;
+                    foundUserStatsGame = true;
+                    break;
+                }
+            }
+
+            if (!foundUserStatsGame)
+            {
+                userSingletonUpdate.GameStatistics.Add(gameStatistics);
+            }
+
+            userSingleton.SetUser(userSingletonUpdate);
         }
 
         List<User> IDal.GetUsers()
