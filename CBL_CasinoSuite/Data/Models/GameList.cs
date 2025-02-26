a08c525d37547b5cd4c5283b909662592e5c4c1d
Aiden Sanders <aidensandersbiz@gmail.com>
2025-02-26 10:58:19 -0700

    Functional Leaderboard

--- CBL_CasinoSuite/Data/Models/GameList.cs
+++ CBL_CasinoSuite/Data/Models/GameList.cs
@@ -1,17 +1,21 @@
 ﻿using CBL_CasinoSuite.Data.Interfaces;
+using CBL_CasinoSuite.Data.NavConstraints;
 
 namespace CBL_CasinoSuite.Data.Models
 {
     public class GameList : IGameList
     {
-        private List<string> _games = new List<string>
-            {
-                "Blackjack",
-            };
+        private List<string> _games = new List<string>();
 
         public List<string> GetGameList()
         {
             return _games;
         }
+    }
+
+    public enum EGameList
+    {
+        None = -1,
+        Blackjack,
     }
 }
