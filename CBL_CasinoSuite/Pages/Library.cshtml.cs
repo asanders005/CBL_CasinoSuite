931b0658092ada1cf98feba627a5a80d8dffba0a
Aiden Sanders <aidensandersbiz@gmail.com>
2025-02-28 13:13:44 -0700

    Library Page Link

--- CBL_CasinoSuite/Pages/Library.cshtml.cs
+++ CBL_CasinoSuite/Pages/Library.cshtml.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using CBL_CasinoSuite.Data.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CBL_CasinoSuite.Pages;
 
 public class Library : PageModel {
+    public List<LibraryGameModel> PageModels { get; set; } = new List<LibraryGameModel>
+    {
+        new LibraryGameModel(EGameList.Blackjack.ToString(), "Play Blackjack", "Image"),
+    };
+
     public void OnGet() {
         
     }
